f643bcfaee96d3f678610516053af8c764380f91
Orso Meneghini <meneghini@fusion.gat.com>
2016-07-19 21:58:19 -0700

    Towards modular c function

---
+++ brainfuse_lib.h
@@ -0,0 +1,3 @@
+
+int load_anns(int nnets, char *argv[]);
+int main(int argc, char *argv[]);
--- compile.sh
+++ compile.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #cp ../brainfuse/brainfuse_run.c ./
-gcc -o brainfuse_run.exe brainfuse_run.c -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
+
+rm -f brainfuse_run.exe
+
+gcc -c brainfuse_lib.c -O3
+gcc -o brainfuse_run.exe brainfuse_run.c brainfuse_lib.o -O3 -L $FANN_ROOT/src -I $FANN_ROOT/src/include/ -lfloatfann -lm
+
+cd tglfnn/samples
+rm -f output.avg
+./run_test.sh
+cat output.avg
