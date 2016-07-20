34cb3ff4a189fa6d4fec79bdcb7b7150aa0145cb
Orso Meneghini <meneghini@fusion.gat.com>
2016-07-19 22:28:19 -0700

    more sheading of brainfuse.exe

--- brainfuse_lib.c
+++ brainfuse_lib.c
@@ -9,16 +9,19 @@ int verbose=1;
 const char *annFile;
 // array of pointers to store all of the anns
 struct fann **anns;
+struct fann_train_data *data_avg, *data_std;
 
-//LOADING ANNS
+//=============
+// LOADING ANNS
+//=============
 int load_anns(int nnets, char *argv[]){
-  unsigned int n;
+  unsigned int n, i, j;
   struct fann *ann;
 
   anns = malloc(nnets * sizeof(struct fann*));
 
+  // Load the network from the file
   for (n = 0; n < nnets; n++){
-      // Load the network from the file
       annFile = argv[n];
       if (verbose) printf("Reading network file %s\n", annFile);
       anns[n] = ann = fann_create_from_file(annFile);
@@ -28,9 +31,24 @@ int load_anns(int nnets, char *argv[]){
       }
   }
 
+  // Allocate memory
+  data_avg = fann_create_train(1, ann->num_input, ann->num_output);
+  data_std = fann_create_train(1, ann->num_input, ann->num_output);
+  for(j = 0; j < ann->num_input; j++){
+    data_avg->input[0][j]=0.;
+    data_std->input[0][j]=0.;
+  }
+  for(j = 0; j < ann->num_output; j++){
+    data_avg->output[0][j]=0.;
+    data_std->output[0][j]=0.;
+  }
+
   return 0;
 }
 
+//============
+// MAIN
+//============
 int main(int argc, char *argv[])
 {
   // Check input consistency
@@ -45,7 +63,6 @@ int main(int argc, char *argv[])
 
   struct fann *ann;
   const char *runFile = argv[argc-1];
-  struct fann_train_data *data_avg, *data_std, *data_lim, *data_nrm;
   fann_type *calc_out, *norm_output, tmp;
   FILE *fp1, *fp2, *fp3, *fp4;
   char dummy[100000];
@@ -56,88 +73,56 @@ int main(int argc, char *argv[])
 
   // Run
   for (n = 0; n < argc-2; n++){
-    ann=anns[n];
 
     if (n==0){
         // Read input data and initialize arrays
         if (verbose)  printf("Reading data from file %s: ", runFile);
         fp1 = fopen(runFile, "r");
         fscanf(fp1, "%u\n", &num_data);
-        if (verbose)  printf("%u runs %d inputs %d ouputs\n", num_data, ann->num_input, ann->num_output);
-        data_avg = fann_create_train(num_data, ann->num_input, ann->num_output);
-        data_std = fann_create_train(num_data, ann->num_input, ann->num_output);
-        data_lim = fann_create_train(num_data, ann->num_input, ann->num_output);
-    	for(i = 0; i < num_data; i++){
-            for(j = 0; j < ann->num_input; j++){
-                fscanf(fp1, FANNSCANF " ", &data_avg->input[i][j]);
-			    data_std->input[i][j]=data_avg->input[i][j];
-			    data_lim->input[i][j]=data_avg->input[i][j];
-		}
-            for(j = 0; j < ann->num_output; j++){
-			    data_avg->output[i][j]=0.;
-			    data_std->output[i][j]=0.;
-			    data_lim->output[i][j]=0.;
-			}
-		}
-	    fclose(fp1);
-     }
+        if (verbose)  printf("%u runs %d inputs %d ouputs\n", num_data, anns[n]->num_input, anns[n]->num_output);
+        for(j = 0; j < anns[n]->num_input; j++){
+          fscanf(fp1, FANNSCANF " ", &data_avg->input[0][j]);
+          data_std->input[0][j]=data_avg->input[0][j];
+        }
+        fclose(fp1);
+    }
 
      //run
-     for(i = 0; i != data_avg->num_data; i++){
-        fann_scale_input( ann, data_avg->input[i] );
-        calc_out = fann_run( ann, data_avg->input[i] );
-        fann_descale_input( ann, data_avg->input[i] );
-        fann_descale_output( ann, calc_out);
-        for(j = 0; j != data_avg->num_output; j++){
-           data_avg->output[i][j] += calc_out[j];
-           data_std->output[i][j] += calc_out[j] * calc_out[j];
-        }
+     fann_scale_input( anns[n], data_avg->input[0] );
+     calc_out = fann_run( anns[n], data_avg->input[0] );
+     fann_descale_input( anns[n], data_avg->input[0] );
+     fann_descale_output( anns[n], calc_out);
+     for(j = 0; j != data_avg->num_output; j++){
+       data_avg->output[0][j] += calc_out[j];
+       data_std->output[0][j] += calc_out[j] * calc_out[j];
      }
   }
 
   // avg and std
-  for(i = 0; i != data_avg->num_data; i++){
-      for(j = 0; j != data_avg->num_output; j++){
-          //std
-          data_std->output[i][j]=sqrt( (data_std->output[i][j]- (data_avg->output[i][j]*data_avg->output[i][j])/(argc-2))/(argc-2) );
-          //avg
-          data_avg->output[i][j]=data_avg->output[i][j]/(argc-2);
-          //rng (NOTE: takes last ANN and not respective ANN, but it's good approx)
-          data_lim->output[i][j]=data_std->output[i][j]/ann->scale_deviation_out[j];
-      }
-      //lim (NOTE: takes last ANN and not respective ANN, but it's good approx)
-      fann_scale_input( ann, data_lim->input[i] );
+  for(j = 0; j != data_avg->num_output; j++){
+      //std
+      data_std->output[i][j]=sqrt( (data_std->output[i][j]- (data_avg->output[i][j]*data_avg->output[i][j])/(argc-2))/(argc-2) );
+      //avg
+      data_avg->output[i][j]=data_avg->output[i][j]/(argc-2);
   }
 
   // print and write
   fp1 = fopen("output.avg", "w");
   fp2 = fopen("output.std", "w");
-  fp3 = fopen("input.lim" , "w");
-  fp4 = fopen("output.rng", "w");
   fprintf(fp1,"%u\n",num_data);
   fprintf(fp2,"%u\n",num_data);
-  fprintf(fp3,"%u\n",num_data);
-  fprintf(fp4,"%u\n",num_data);
   for(i = 0; i < data_avg->num_data; i++){
       if (verbose)  printf("Run %d: ",i);
-      for(j = 0; j < data_avg->num_input; j++){
-        fprintf(fp3,"%f ",data_lim->input[i][j]);
-      }
       for(j = 0; j < data_avg->num_output; j++){
           if (verbose)  printf("%f (%f) ",data_avg->output[i][j],data_std->output[i][j] );
           fprintf(fp1,"%f ",data_avg->output[i][j]);
           fprintf(fp2,"%f ",data_std->output[i][j]);
-          fprintf(fp4,"%f ",data_lim->output[i][j]);
       }
       if (verbose)  printf("\n");
-      fprintf(fp3,"\n");
       fprintf(fp1,"\n");
       fprintf(fp2,"\n");
-      fprintf(fp4,"\n");
   }
   fclose(fp1);
   fclose(fp2);
-  fclose(fp3);
-  fclose(fp4);
   return 0;
 }
