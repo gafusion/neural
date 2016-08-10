0c1d6e7ffbd2f270a741aaee5aeb2521190ce3ea
Orso Meneghini <meneghini@fusion.gat.com>
2016-08-09 17:07:32 -0700

    Reinstated power-law normalization

--- brainfuse_lib.c
+++ brainfuse_lib.c
@@ -7,15 +7,16 @@
 #include <dirent.h>
 
 static unsigned int n_models=2; //number of nn physics models
-static unsigned int verbose=0;
+static unsigned int verbose=1;
 
 // arrays of pointers storing multiple ANNS instances,
 // of multiple ANNS ensembles, for different physics models
 unsigned int *nanns=NULL;
 unsigned int *loaded_anns=NULL;
 struct fann ***anns;
-struct fann_train_data **data_avg, **data_std;
+struct fann_train_data **data_avg, **data_std, **data_nrm;
 int model;
+fann_type ****nrm_table;
 
 //=============
 // LOADING ANNS
@@ -23,9 +24,13 @@ int model;
 
 int load_anns(int global_nn_model, char *directory, char *basename){
   DIR *dir;
-  int n,j;
+  int n,k,j;
   struct dirent *ent;
   char annFile[2000];
+  char dummy[100000];
+  char * pch;
+  FILE *fp2;
+  fann_type tmp;
 
   model=global_nn_model;
 
@@ -35,6 +40,8 @@ int load_anns(int global_nn_model, char *directory, char *basename){
     anns = malloc(n_models * sizeof(struct fann**));
     data_avg = malloc(n_models * sizeof(struct fann_train_data *));
     data_std = malloc(n_models * sizeof(struct fann_train_data *));
+    data_nrm = malloc(n_models * sizeof(struct fann_train_data *));
+    nrm_table = malloc(n_models * sizeof(fann_type***));
   }
 
   if(verbose) printf("NN model %d\n",model);
@@ -83,11 +90,46 @@ int load_anns(int global_nn_model, char *directory, char *basename){
     // Allocate memory for data
     data_avg[model] = fann_create_train(1, anns[model][0]->num_input, anns[model][0]->num_output);
     data_std[model] = fann_create_train(1, anns[model][0]->num_input, anns[model][0]->num_output);
+    data_nrm[model] = fann_create_train(1, anns[model][0]->num_input, anns[model][0]->num_output);
+    nrm_table[model] = malloc(nanns[model] * sizeof(fann_type**));
+    for(n=0; n<nanns[model]; n++){
+        nrm_table[model][n] = malloc(anns[model][0]->num_input * sizeof(fann_type*));
+        for(k=0; k<anns[model][0]->num_input; k++){
+            nrm_table[model][n][k] = malloc(anns[model][0]->num_output * sizeof(fann_type));
+        }
+    }
 
+    // Power-law outputs normalization
+    n=0;
+    dir = opendir(directory);
+    while ((ent = readdir (dir)) != NULL) {
+      if (strncmp(ent->d_name,basename,9)==0){
+        sprintf(annFile, "%s/%s", directory,ent->d_name);
+        fp2 = fopen(annFile,"r");
+        for(k = 0; k < 47; k++){
+          fgets(dummy,100000,fp2);
+        }
+        if (strstr(dummy, "norm_output=") != NULL){
+            pch = strtok(dummy+12," \n");
+            for(j = 0; j < anns[model][0]->num_output; j++){
+                for(k = 0; k < anns[model][0]->num_input; k++){
+                   nrm_table[model][n][k][j]=(fann_type)atof(pch);
+                   pch = strtok (NULL, " \n");
+                }
+            }
+        }
+        fclose(fp2);
+        if (verbose) printf("%d Setting normalization %s\n",n, annFile);
+        n+=1;
+      }
+    }
+    closedir (dir);
+
+    // Set loaded flag
     loaded_anns[model]=1;
   }
 
-  //initialize memory
+  // Initialize memory
   for(j = 0; j < anns[model][0]->num_input; j++){
     data_avg[model]->input[0][j]=0.;
     data_std[model]->input[0][j]=0.;
@@ -118,6 +160,7 @@ int load_anns_inputs(fann_type *data_in){
     if (verbose) printf("in  %02d: %3.3f\n",j+1,data_in[j]);
     data_avg[model]->input[0][j]=(fann_type)data_in[j];
     data_std[model]->input[0][j]=data_avg[model]->input[0][j];
+    data_nrm[model]->input[0][j]=data_avg[model]->input[0][j];
   }
   if (verbose) printf("\n");
   return 0;
@@ -135,28 +178,42 @@ int load_anns_inputs__(fann_type *data_in){
 // RUNNING ANNS
 //=============
 int run_anns(){
-  unsigned int n, j;
+  unsigned int n,k,j;
   fann_type *calc_out;
 
   if (verbose) printf("Running ANNs\n");
 
-  //run
+  // run
   for (n = 0; n < nanns[model]; n++){
+
+     // power law normalization
+     for(j = 0; j < anns[model][0]->num_output; j++){
+        data_nrm[model]->output[0][j]=1.;
+        for(k = 0; k < anns[model][0]->num_input; k++){
+            if (nrm_table[model][n][k][j]!=0.0){
+                data_nrm[model]->output[0][j]*=pow(data_nrm[model]->input[0][k],nrm_table[model][n][k][j]);
+             }
+        }
+     }
+
+     // scale - run - descale
      fann_scale_input( anns[model][n], data_avg[model]->input[0] );
      calc_out = fann_run( anns[model][n], data_avg[model]->input[0] );
      fann_descale_input( anns[model][n], data_avg[model]->input[0] );
      fann_descale_output( anns[model][n], calc_out);
+
+     // avg and std (part 1)
      for(j = 0; j != data_avg[model]->num_output; j++){
-       data_avg[model]->output[0][j] += calc_out[j];
-       data_std[model]->output[0][j] += calc_out[j] * calc_out[j];
+       data_avg[model]->output[0][j] += calc_out[j] * data_nrm[model]->output[0][j];
+       data_std[model]->output[0][j] += calc_out[j] * data_nrm[model]->output[0][j] * calc_out[j] * data_nrm[model]->output[0][j];
      }
   }
 
-  // calculate avg and std
+  // calculate avg and std (part 2)
   for(j = 0; j != data_avg[model]->num_output; j++){
-      //std
+      // std
       data_std[model]->output[0][j]=sqrt( (data_std[model]->output[0][j] - (data_avg[model]->output[0][j]*data_avg[model]->output[0][j])/nanns[model])/nanns[model] );
-      //avg
+      // avg
       data_avg[model]->output[0][j]=data_avg[model]->output[0][j]/nanns[model];
   }
 
